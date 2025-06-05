3814c83d64fd132b4a84dbdba7bf60a3ed019179
julisa <julisa.leon344@gmail.com>
2025-06-04 22:18:19 -0400

    refactor: Remove nix-vscode-extensions overlay from HM to resolve collision

--- flake.nix
+++ flake.nix
@@ -1,19 +1,19 @@
-# ~/nixos-config/flake.nix - LA VERSIÓN FINAL
+# ~/nixos-config/flake.nix
 {
   description = "Configuracion de NixOS para Julisa";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    
-    # --- ESTA ES LA LÍNEA QUE CAMBIAMOS ---
     home-manager.url = "github:nix-community/home-manager/release-24.05";
-    
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+    # We still need nix-vscode-extensions if pkgsUnstable.vscode-extensions doesn't have everything
+    # or if you prefer its versions. For now, let's see if pkgsUnstable is enough.
+    # If not, we'll add this back and use its specific attributes carefully.
+    # nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, nix-vscode-extensions, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, /*nix-vscode-extensions,*/ ... }@inputs:
   let
     system = "x86_64-linux";
     pkgsUnstable = import nixpkgs-unstable { inherit system; config.allowUnfree = true; };
@@ -21,7 +21,6 @@
   in
   {
     nixosConfigurations."julixos" = nixpkgs.lib.nixosSystem {
-      # ... el resto del archivo es idéntico y correcto ...
       inherit system;
       specialArgs = {
         inherit inputs pkgsUnstable;
@@ -33,12 +32,14 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "bak";
           home-manager.extraSpecialArgs = {
-            inherit pkgsUnstable;
+            inherit pkgsUnstable; // pkgsUnstable will be available in home.nix
             blesh = pkgsStable.blesh.src;
           };
           home-manager.users.julisa = import ./users/julisa/home.nix;
-          nixpkgs.overlays = [ nix-vscode-extensions.overlays.default ];
+          # LA SIGUIENTE LÍNEA HA SIDO ELIMINADA/COMENTADA:
+          # nixpkgs.overlays = [ nix-vscode-extensions.overlays.default ];
         }
       ];
     };
